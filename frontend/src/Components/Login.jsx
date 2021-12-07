bf3059c7616c56f40236f162c72d93e5ba792d26
Jessica Cheung <cheungje@usc.edu>
2021-12-07 00:04:54 -0800

    Routed to Swipe

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 import {
   BrowserRouter as Router,
@@ -12,7 +12,6 @@ import {
 import "./Login.scss";
 
 const Login = () => {
-
   const [message, setMessage] = React.useState("");
   const [valid, setValid] = React.useState(false);
 
@@ -23,43 +22,42 @@ const Login = () => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const handleUserChange = event => setUsername(event.target.value);
-  const handlePasswordChange = event => setPassword(event.target.value);
- 
+  const handleUserChange = (event) => setUsername(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+
   async function sampleFunc() {
     let response = await fetch(`/api/student/${username}/${password}`);
     let body = await response.json();
-   
-    if(body === true){
+
+    if (body === true) {
       setValid(true);
       //console.log("GOTTTTINNN")
-    }
-    else{
+    } else {
       setValid(false);
     }
     //console.log("valid = " + valid);
   }
 
-  React.useEffect(() => { //only happens once valid changes, since it takes a bit for valid to change
+  React.useEffect(() => {
+    //only happens once valid changes, since it takes a bit for valid to change
 
-    if(valid === true){ //if false then set correct error message
-      navigate("/Register")
+    if (valid === true) {
+      //if false then set correct error message
+      navigate("/Swipe", { state: { username: username } });
       setMessage("");
     }
-    }, [valid])
+  }, [valid]);
 
-
-  const navigate = useNavigate() //used to go to register page
-
+  const navigate = useNavigate(); //used to go to register page
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const toInput = {username, password};
+    const toInput = { username, password };
     sampleFunc(toInput);
-    setUsername("");
-    setPassword("");
+    //setUsername("");
+    //setPassword("");
 
-    if(valid === false){
+    if (valid === false) {
       invalid = true;
       setMessage("Invalid Username or Password"); //display in return
     }
@@ -99,12 +97,21 @@ const Login = () => {
               required
             />
           </div>
-          <br />          
-          <button type="button" onClick={handleSubmit} id="login" className="submitButton">
+          <br />
+          <button
+            type="button"
+            onClick={handleSubmit}
+            id="login"
+            className="submitButton"
+          >
             Login
           </button>
-          {<p style={{ margin: 7 }} id="error-message"> {message}</p>}
-
+          {
+            <p style={{ margin: 7 }} id="error-message">
+              {" "}
+              {message}
+            </p>
+          }
         </form>
         <br />
         <div id="DNE">
