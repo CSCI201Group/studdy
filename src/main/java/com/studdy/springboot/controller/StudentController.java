a338eddb7ee4a96f80347b5480e80e04d7c76e72
Edward Lin <eddiel@usc.edu>
2021-12-07 23:53:19 -0800

    add no longer calls save matches, just saves

--- src/main/java/com/studdy/springboot/controller/StudentController.java
+++ src/main/java/com/studdy/springboot/controller/StudentController.java
@@ -83,7 +83,7 @@ public class StudentController {
 		}
 		s1.getMatch().add(s2);
 
-		saveMatches(e1);
+		s1.saveMatches();
 		save(s1);
 	}
 
@@ -100,7 +100,7 @@ public class StudentController {
 		}
 		s1.getMatch().addReject(s2);
 
-		saveRejected(e1);
+		s1.saveRejected();
 		save(s1);
 	}
 
